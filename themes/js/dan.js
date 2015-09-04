d6738e7e4389a9a0029d769d17d43574731c1e41
DanMburu <danstjoy@gmail.com>
2015-09-04 09:06:35 +0300

    Init

---
+++ themes/js/dan.js
@@ -0,0 +1,32 @@
+
+$( document ).bind( "mobileinit", function() {
+    // Make your jQuery Mobile framework configuration changes here!
+    $.support.cors = true;
+    $.mobile.allowCrossDomainPages = true; 
+});
+
+
+$(document).on("pageshow", "#landing", function () { // When entering pagetwo
+ showLoader();
+setTimeout(function(){
+	  //window.location = $('#RootUrl').val();
+	
+ }, 1000);
+});
+
+
+
+
+function showLoader(){
+ $.mobile.loading( "show", {
+		text: '',
+		textVisible: false,
+		theme: 'a',
+		textonly: false,
+		html: ''
+	 });
+	 $('.ui-icon-loading').addClass('ui-icon');
+}
+function hideLoader(){
+	 $.mobile.loading( "hide" );
+}
